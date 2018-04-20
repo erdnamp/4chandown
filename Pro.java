23b23fc57bceba9e5fc3e3ab55d4757b739b8424
erdnamp <38553706+erdnamp@users.noreply.github.com>
2018-04-20 20:10:46 +0100

    Add files via upload

---
+++ Pro.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author User
+ */
+public class Pro extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sick.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ SickController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pro;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class SickController implements Initializable {
+    @FXML
+    private TextField url;
+    @FXML
+    private TextField output;
+    @FXML
+    private void browse(ActionEvent event){
+    try{
+    DirectoryChooser dirChooser = new DirectoryChooser();
+    dirChooser.setTitle("Choose Folder");
+    File file = dirChooser.showDialog(null);
+    System.out.println(file);
+    output.setText(file.getPath().concat("\\"));
+    }catch(Exception error){
+        
+    }
+    }
+    @FXML
+    private void download(ActionEvent event) throws IOException{
+    Document doc = Jsoup.connect(url.getText()).get();
+    Elements images=doc.getElementsByAttributeValue("class", "fileThumb");
+    for(int i=0;i<images.size();i++){
+        String image=images.get(i).attr("href");
+        String imags="https:"+image;
+        URL x = new URL(imags);
+        File file=new File(output.getText().concat(image.substring(15)));
+        FileUtils.copyURLToFile(x,file);
+    }
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
